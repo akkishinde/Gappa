966c6f6329c4c32f98cdf058edcc1f96f0e9b73e
Akshay <akshay11.shinde@hotmail.com>
2015-08-25 22:36:52 +0530

    LoginPageUI

--- app/src/main/java/com/akshay/gappa/MainActivity.java
+++ app/src/main/java/com/akshay/gappa/MainActivity.java
@@ -1,16 +1,36 @@
 package com.akshay.gappa;
 
+import android.app.Activity;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
+
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login=(Button) findViewById(R.id.button);
+        EditText username=(EditText)findViewById(R.id.editText);
+        EditText password=(EditText)findViewById(R.id.editText2);
+        TextView register=(TextView)findViewById(R.id.register_here);
+        ImageView imageView=(ImageView)findViewById(R.id.imageView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"Comic_White_Rabbit_.otf");
+        login.setText("Sign In");
+        login.setTypeface(typeface);
+        register.setTypeface(typeface);
+
+
     }
 
     @Override
