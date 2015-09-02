faa9bd64bdc647a7290947875198baa80db58201
Akshay <akshay11.shinde@hotmail.com>
2015-09-02 10:27:39 +0530

    Added New Fragment for Swipe Views

---
+++ app/src/main/java/com/akshay/gappa/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.akshay.gappa;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button login=(Button) findViewById(R.id.button);
+        EditText username=(EditText)findViewById(R.id.editText);
+        EditText password=(EditText)findViewById(R.id.editText2);
+        TextView register=(TextView)findViewById(R.id.register_here);
+        ImageView imageView=(ImageView)findViewById(R.id.imageView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"Comic_White_Rabbit_.otf");
+        login.setText("Sign In");
+        login.setTypeface(typeface);
+        register.setTypeface(typeface);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
